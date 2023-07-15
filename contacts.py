b4ca852cf965cef580c8cf9e6017f468cdf3de96
Diego Martinez <martinez.diego90@gmail.com>
2023-07-15 20:47:41 +0200

    add json prints

--- contacts.py
+++ contacts.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def contacts(url):
@@ -11,21 +12,39 @@ def contacts(url):
 # Busco los primeros dos divs con esa clase
     divs = soup.find_all("div", class_="contact_item")
 
+# Creo una lista para almacenar los datos
+    data = []
+
 # Itero extrayendo el h3 de cada div
     for div in divs:
         title = div.h3.text
-        print(title)
 
     # Extraigo los anchor que comienzan con 'mailto:'
         links = div.find_all("a", href=lambda href: href and href.startswith("mailto:"))
         
+        mails_data = []
+
     # Itero tomando los links, eliminando el 'mailto:' del principio y lo posterior a '.com'    
         for link in links:
             mail = link["href"].replace("mailto:", "")
             mail = mail.split(".com")[0] + ".com"
         
-            print(mail)
+            mails_data = {
+                'Mail': mail
+            }
+            mails_data.append(mails_data)
             
-        print('.')
+        title_data = {
+            'Title': title,
+            'MailsData': mails_data
+        }
+
+        data.append(title_data)    
+
+# 
+    with open('contacts.json', 'w') as file:
+        json.dump(data, file, indent=6)
+
+print('Datos guardados en datos.json')
 
 contacts("https://reviewpro.shijigroup.com/team#contact")
--- products.py
+++ products.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def products(url):
@@ -11,14 +12,18 @@ def products(url):
 # Busco los primeros dos divs con esa clase
     divs = soup.find_all("div", class_="nav_dropdown-block")[:2]
 
+# Creo una lista para almacenar los datos
+    data = []
+
 # Itero para extraer de cada div, el texto que hace de título
     for div in divs:
         title = div.find("div", class_="text-weight-bold").text
-        print(title)
-        print("-")
 
     # Dentro extraigo todos los anchor del segundo div
         subtitles = div.find_all("a")
+
+    # Creo otra lista que estará contenida dentro de data
+        subtitles_data = []
 
     # Itero extrayendo el texto y el link, en el caso de los links que empiezan con "/" les agrego la web
         for subtitle in subtitles:
@@ -26,10 +31,26 @@ def products(url):
             link = subtitle["href"]
             if not link.startswith("https"):
                 link = "https://reviewpro.shijigroup.com" + link
+        # Indico qué elementos iran dentro de la sublista
+            subtitle_data = {
+                'Subtitle': sub,
+                'Link': link
+                }
+            subtitles_data.append(subtitle_data)
 
-            print(sub)
-            print(link)
+    # Indico como irá estructurada la lista inicial
+        title_data = {
+            'Title': title,
+            'Content': subtitles_data
+        }
 
-        print('.')
+        data.append(title_data)
+
+# 
+    with open('products.json', 'w') as file:
+        json.dump(data, file, indent=6)
+
+print('Datos guardados en datos.json')
+
 
 products("https://reviewpro.shijigroup.com/")
